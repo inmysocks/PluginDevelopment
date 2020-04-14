11918b6dff10f4ced29f3f8422aa2a2bdb6a950d
inmysocks <inmysocks@gmail.com>
2020-04-14 11:15:26 +0200

    maybe this will do it

--- PatchMakeLibrary.sh
+++ PatchMakeLibrary.sh
@@ -9,6 +9,6 @@ NEW2="var themesPaths = \$tw.getLibraryItemSearchPaths(\$tw.config.themesPath,\$
 ORIGINAL3="collectPlugins(path.resolve(\$tw.boot.corePath,\$tw.config.languagesPath))\;"
 NEW3="var languagesPaths = \$tw.getLibraryItemSearchPaths(\$tw.config.languagesPath,\$tw.config.languagesEnvVar);for(var u=0; u<languagesPaths.length; u++) {collectPlugins(languagesPaths[u]);}"
 
-sed --in-place="out.bak" "s/$ORIGINAL1/$NEW1/g" $(pwd)/TiddlyWiki5/core/modules/commands/makelibrary.js
-sed --in-place="out.bak" "s/$ORIGINAL2/$NEW2/g" $(pwd)/TiddlyWiki5/core/modules/commands/makelibrary.js
-sed --in-place="out.bak" "s/$ORIGINAL3/$NEW3/g" $(pwd)/TiddlyWiki5/core/modules/commands/makelibrary.js
+sed -i.bak "s/$ORIGINAL1/$NEW1/g" $(pwd)/TiddlyWiki5/core/modules/commands/makelibrary.js
+sed -i.bak "s/$ORIGINAL2/$NEW2/g" $(pwd)/TiddlyWiki5/core/modules/commands/makelibrary.js
+sed -i.bak "s/$ORIGINAL3/$NEW3/g" $(pwd)/TiddlyWiki5/core/modules/commands/makelibrary.js
